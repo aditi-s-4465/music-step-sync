52a6cd17746b759f9d4adc9b5d78fce7d12c2046
ethanbg2 <ethanbg2@illinois.edu>
2024-01-14 00:43:07 -0600

    add buttons to all routes

--- src/app/choose-music.js
+++ src/app/choose-music.js
@@ -1,5 +1,39 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Colors } from "../styles";
+import { Link } from "expo-router";
 
 export default function Home() {
-  return <Text>Choose Music</Text>;
+  return (
+    <View style={styles.container}>
+      <Link href="/workout" asChild>
+        <Pressable style={styles.startButton}>
+          <Text
+            style={{
+              fontSize: 20,
+              color: Colors.AppTheme.colors.text,
+            }}
+          >
+            Start Workout
+          </Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.AppTheme.colors.background,
+    alignItems: "center",
+  },
+  startButton: {
+    marginTop: 50,
+    backgroundColor: Colors.AppTheme.colors.primary,
+    borderRadius: 5,
+    width: 200,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
