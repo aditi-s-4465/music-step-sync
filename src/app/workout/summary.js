f604a0b06952232fee66caef71c5f8ede71b7157
ethanbg2 <ethanbg2@illinois.edu>
2024-01-14 00:33:32 -0600

    add expo router

---
+++ src/app/workout/summary.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function WorkoutSummary() {
+  return <Text>workout summary page</Text>;
+}
---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export const AppTheme = {
+  dark: true,
+  colors: {
+    primary: "rgba(51, 151, 104, 1)",
+    background: "rgba(28, 43, 33, 1)",
+    card: "rgba(6, 95, 70, 0.65)",
+    text: "rgba(255, 255, 255, 1)",
+    border: "rgba(6, 95, 70, 0.65)",
+    notification: "rgba(6, 95, 70, 0.65)",
+  },
+};
