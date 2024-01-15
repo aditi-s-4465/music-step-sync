5da90ae9e652b864943ea3530bb704456b4aaf8e
ethanbg2 <ethanbg2@illinois.edu>
2024-01-14 20:38:01 -0600

    moved spotify integration file to choose music

--- src/app/choose-music.js
+++ src/app/choose-music.js
@@ -1,10 +1,56 @@
 import { Text, View, StyleSheet, Pressable } from "react-native";
 import { Colors } from "../styles";
 import { Link } from "expo-router";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { useEffect } from "react";
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token",
+};
 
 export default function ChooseMusic() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: "client_id",
+      scopes: ["user-read-email", "playlist-modify-public"],
+      // To follow the "Authorization Code Flow" to fetch token after
+      // authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: makeRedirectUri(),
+    },
+    // eslint-disable-next-line prettier/prettier
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      // const { code } = response.params;
+      console.log(response);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
+      <Pressable
+        onPress={() => promptAsync()}
+        style={{
+          marginTop: 50,
+          justifyContent: "center",
+          backgroundColor: Colors.AppTheme.colors.primary,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 10,
+            color: Colors.AppTheme.colors.text,
+          }}
+        >
+          Connect to Spotify
+        </Text>
+      </Pressable>
       <Link href="/workout" asChild>
         <Pressable style={styles.startButton}>
           <Text
