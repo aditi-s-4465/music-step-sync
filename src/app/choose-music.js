37ac1b589da4a11e9f58bf84d5e3d378a1d3f0f1
ethanbg2 <ethanbg2@illinois.edu>
2024-02-20 22:22:30 -0600

    add token retrieval

--- src/app/choose-music.js
+++ src/app/choose-music.js
@@ -3,32 +3,68 @@ import { Colors } from "../styles";
 import { Link } from "expo-router";
 import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
 import { useEffect } from "react";
+import { Buffer } from "buffer";
+import { spotifyConst } from "../const";
 
-// Endpoint
 const discovery = {
   authorizationEndpoint: "https://accounts.spotify.com/authorize",
   tokenEndpoint: "https://accounts.spotify.com/api/token",
 };
 
 export default function ChooseMusic() {
-  const [request, response, promptAsync] = useAuthRequest(
+  const [_, response, promptAsync] = useAuthRequest(
     {
-      clientId: "client_id",
-      scopes: ["user-read-email", "playlist-modify-public"],
+      clientId: spotifyConst.clientId,
+      scopes: spotifyConst.scopes,
       // To follow the "Authorization Code Flow" to fetch token after
       // authorizationEndpoint
       // this must be set to false
       usePKCE: false,
-      redirectUri: makeRedirectUri(),
+      redirectUri: makeRedirectUri({ path: "choose-music" }),
     },
     // eslint-disable-next-line prettier/prettier
     discovery
   );
 
+  const getAccessToken = async (code, refresh = false) => {
+    let body;
+    if (refresh) {
+      body = new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: code,
+      });
+    } else {
+      body = new URLSearchParams({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: makeRedirectUri({ path: "choose-music" }),
+      });
+    }
+    try {
+      const res = await fetch(discovery.tokenEndpoint, {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(
+              spotifyConst.clientId + ":" + spotifyConst.clientSecret
+            ).toString("base64"),
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: body.toString(),
+      });
+
+      const responseJson = await res.json();
+      console.log(responseJson);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     if (response?.type === "success") {
-      // const { code } = response.params;
-      console.log(response);
+      const { code } = response.params;
+      getAccessToken(code);
     }
   }, [response]);
 
@@ -38,6 +74,7 @@ export default function ChooseMusic() {
         onPress={() => promptAsync()}
         style={{
           marginTop: 50,
+          height: 60,
           justifyContent: "center",
           backgroundColor: Colors.AppTheme.colors.primary,
         }}
--- src/const.js
+++ src/const.js
@@ -1,2 +1,17 @@
 // defines how often steps per minute is recalculated in milliseconds
 export const spmUpdateInterval = 5000;
+
+export const spotifyConst = {
+  clientId: "6f5d10838a144c989f21c3a3c76484cb",
+  clientSecret: "",
+  scopes: [
+    "streaming",
+    "user-library-read",
+    "user-top-read",
+    "user-read-recently-played",
+    "playlist-read-private",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-playing",
+  ],
+};
